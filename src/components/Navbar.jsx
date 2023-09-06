386f842b30d09b3dded2a16dc199862d445c2966
Ed <edsagoefvr@gmail.com>
2023-09-06 04:32:05 +0000

    app setup: directories etc

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/main.css'
+
+export default function Navbar () {
+    return (
+        <div>
+            <nav className='nav'>
+                <img src='/airbnb-logo.png' />
+            </nav>
+        </div>
+    )
+}
