ee7d5c7d37a8592ef579dc087275a1f18e50f593
Ed <edsagoefvr@gmail.com>
2023-09-09 21:17:40 +0000

    Adding badges with conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ export default function App() {
       location={item.location}
       title={item.title}
       price={item.price}
+      openSpots={item.openSpots}
       />
     )
   })
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,26 @@
 import React from "react"
 
 export default function Card(props) {
+    let badgeText
+
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
-           <img src={props.img} className="card--image"/>
-           <div className="card--stats">
+            {badgeText && <div className="badge">{badgeText}</div>}
+            <img src={props.img} className="card--image"/>
+            <div className="card--stats">
                 <img src="star.png" className="card--star" />
                 <span className="grayed-out">{props.rating}</span>
                 <span className="grayed-out">({props.reviewCount}) • </span>
                 <span className="grayed-out">{props.location}</span>
-           </div>
-           <p>{props.title}</p>
-           <p><span className="bold">From ${props.price}</span> / person</p>
+            </div>
+            <p>{props.title}</p>
+            <p><span className="bold">From ${props.price}</span> / person</p>
         </div>
     )
 }
